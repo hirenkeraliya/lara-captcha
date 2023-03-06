267f6b60d8918fc4b76127c43b0f2727b83c63cf
Hiren <hirenkeradiya@gmail.com>
2023-03-06 21:48:57 +0530

    Edit key name

--- src/config/lara_captcha.php
+++ src/config/lara_captcha.php
@@ -3,7 +3,7 @@
 return [
     'enabled' => env('LARA_CAPTCHA_ENABLED', true),
 
-    'input_name' => 'laravel',
+    'input_name' => 'captcha',
 
     'error' => [
         'message' => 'Captcha not valid.',
